9e0f1791969b32ea2714894e4c90bae08cf4d207
afx-nicolas <afxnicolas@gmail.com>
2021-08-22 17:17:15 -0300

    Solve Exercise 03

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -11,16 +11,94 @@ function $query(el) {
   return element;
 }
 
+const url = "http://localhost:3333/cars";
+
 const req = {
   get: {
     method: "GET",
   },
+  post: (car) => {
+    return {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        image: car.image,
+        brandModel: car.brandModel,
+        year: car.year,
+        plate: car.plate,
+        color: car.color,
+      }),
+    };
+  },
 };
 
+async function refreshTable() {
+  const cars = await getCars();
+  tableBody.innerHTML = "";
+
+  if (cars.length) {
+    cars.forEach((car) => {
+      const tr = $create("tr");
+      car.forEach((element) => {
+        const td = elementTypes[element.type](element.value);
+        tr.appendChild(td);
+      });
+      tableBody.appendChild(tr);
+    });
+  } else {
+    tableBody.innerHTML = `
+      <tr>
+        <td colspan="5"><h2>Nenhum carro encontrado.</h2></td>
+      <tr>
+    `;
+  }
+}
+
+function showMsg(msg) {
+  const errorText = $query("error");
+  if (msg.error) {
+    if (errorText.classList.contains("hide")) {
+      errorText.classList.remove("hide");
+    }
+    errorText.textContent = msg.message;
+  } else {
+    if (!errorText.classList.contains("hide")) {
+      errorText.classList.add("hide");
+    }
+  }
+}
+
 async function getCars() {
-  return fetch("http://localhost:3333/cars", req.get)
+  const cars = fetch(url, req.get)
     .then((res) => res.json())
     .then((res) => res);
+
+  return formatCar(await cars);
+}
+
+async function postCar(car) {
+  const result = fetch(url, req.post(car))
+    .then((response) => response.json())
+    .then((res) => showMsg(res));
+
+  refreshTable();
+}
+
+function formatCar(cars) {
+  const carsObject = [];
+  cars.map((car) => {
+    const carArray = [
+      { type: "image", value: car.image },
+      { type: "text", value: car.brandModel },
+      { type: "text", value: car.year },
+      { type: "text", value: car.plate },
+      { type: "color", value: car.color },
+    ];
+    carsObject.push(carArray);
+  });
+  return carsObject;
 }
 
 const form = $query("car-form");
@@ -50,27 +128,29 @@ function createText(value) {
 function createColor(value) {
   const td = $create("div");
   td.style.width = "100%";
-  td.style.height = "100px";
+  td.style.height = "128px";
   td.style.backgroundColor = value;
   return td;
 }
 
-async function refreshTable() {
-  const cars = await getCars();
+const getFormElement = (e) => (elementName) => {
+  return e.target.elements[elementName];
+};
 
-  if (cars.length) {
-    // console.log(cars);
-    cars.forEach((car) => {
-      const tr = $create("tr");
-      car.forEach((element) => {
-        const td = elementTypes[element.type](element.value);
-        tr.appendChild(td);
-      });
-      tableBody.appendChild(tr);
-    });
-  } else {
-    table.innerHTML = "<h2>Nenhum carro encontrado.</h2>";
-  }
-}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const getElement = getFormElement(e);
+  const car = {
+    image: getElement("image").value,
+    brandModel: getElement("brand-model").value,
+    year: getElement("year").value,
+    plate: getElement("plate").value,
+    color: getElement("color").value,
+  };
+
+  form.reset();
+  form.image.focus();
+  postCar(car);
+});
 
 refreshTable();
